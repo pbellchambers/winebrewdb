e7ff46bfec748b96fb196d27714682726a6f76d3
Paul Bellchambers <pbellchambers@users.noreply.github.com>
2012-03-04 21:22:41 +0000

    Set Java compatibility to 1.6+, fixed linux font issue (hopefully), also fixed issue displaying some brew pictures.

--- winebrewdb/src/com/pori/WineBrewDB/MainWindow.java
+++ winebrewdb/src/com/pori/WineBrewDB/MainWindow.java
@@ -1,6 +1,7 @@
 package com.pori.WineBrewDB;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Image;
 import java.io.IOException;
 import java.net.URL;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class MainWindow {
 
@@ -21,10 +23,43 @@ public class MainWindow {
 
 	/**
 	 * Launch the application.
-	 */
+	 */	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				Font defaultFont = new Font("Arial",Font.PLAIN,12);
+				UIManager.put("Button.font", defaultFont);				
+				UIManager.put("ToggleButton.font", defaultFont);
+				UIManager.put("RadioButton.font", defaultFont);
+				UIManager.put("CheckBox.font", defaultFont);
+				UIManager.put("ColorChooser.font", defaultFont);
+				UIManager.put("ComboBox.font", defaultFont);
+				UIManager.put("Label.font", defaultFont);
+				UIManager.put("List.font", defaultFont);
+				UIManager.put("MenuBar.font", defaultFont);
+				UIManager.put("MenuItem.font", defaultFont);
+				UIManager.put("RadioButtonMenuItem.font", defaultFont);
+				UIManager.put("CheckBoxMenuItem.font", defaultFont);
+				UIManager.put("Menu.font", defaultFont);
+				UIManager.put("PopupMenu.font", defaultFont);
+				UIManager.put("OptionPane.font", defaultFont);
+				UIManager.put("Panel.font", defaultFont);
+				UIManager.put("ProgressBar.font", defaultFont);
+				UIManager.put("ScrollPane.font", defaultFont);
+				UIManager.put("Viewport.font", defaultFont);
+				UIManager.put("TabbedPane.font", defaultFont);
+				UIManager.put("Table.font", defaultFont);
+				UIManager.put("TableHeader.font", defaultFont);
+				UIManager.put("TextField.font", defaultFont);
+				UIManager.put("PasswordField.font", defaultFont);
+				UIManager.put("TextArea.font", defaultFont);
+				UIManager.put("TextPane.font", defaultFont);
+				UIManager.put("EditorPane.font", defaultFont);
+				UIManager.put("TitledBorder.font", defaultFont);
+				UIManager.put("ToolBar.font", defaultFont);
+				UIManager.put("ToolTip.font", defaultFont);
+				UIManager.put("Tree.font", defaultFont);
+				
 				try {
 					window = new MainWindow();
 					MainWindow.WineBrewDBFrame.setVisible(true);
