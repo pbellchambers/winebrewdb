b336929a501d2028424b54fbe32259889e08fe7b
Paul Bellchambers <pbellchambers@users.noreply.github.com>
2012-03-05 17:25:39 +0000

    Recipe search/add/edit done.

---
+++ winebrewdb/src/com/pori/WineBrewDB/RecipeAddPanel.java
@@ -0,0 +1,252 @@
+package com.pori.WineBrewDB;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+import com.pori.WineBrewDB.SQLite.DBEngine;
+
+import net.miginfocom.swing.MigLayout;
+
+public class RecipeAddPanel extends JPanel {
+
+	private static final long serialVersionUID = -4501679200598952188L;
+	public static JPanel tabbedRecipeAddPanel;public static JLabel lblRecipeNameAdd;
+	public static JTextField textRecipeNameAdd;
+	public static JLabel lblInspirationAdd;
+	public static JTextField textInspirationAdd;
+	public static JLabel lblIngredientsAdd;
+	public static JTextArea textIngredientsAdd;
+	public static JLabel lblSuggestedYeastAdd;
+	public static JTextField textSuggestedYeastAdd;
+	public static JLabel lblMethodAdd;
+	public static JTextArea textMethodAdd;
+	public static JLabel lblNotesAdd;
+	public static JTextArea textNotesAdd;
+	public static JLabel lblReferencesAdd;
+	public static JTextField textReferencesAdd;
+	public static JLabel lblVolumeAdd;
+	public static JTextField textVolumeAdd;
+	public static JLabel lblRecipeRefAdd;
+	public static JTextField textRecipeRefAdd;
+	public static JScrollPane RecipeAddIngredientsScrollPane;
+	public static JScrollPane RecipeAddMethodScrollPane;
+	public static JScrollPane RecipeAddNotesScrollPane;
+	public static JButton btnRecipeAddEdit;
+	public static JButton btnRecipeAddCancel;
+	public static JButton btnRecipeAddSave;
+	
+	
+	public static void InitializePanel(){
+		
+		tabbedRecipeAddPanel = new JPanel();
+		tabbedRecipeAddPanel.setBackground(Color.WHITE);
+		tabbedRecipeAddPanel.setLayout(new MigLayout("", "[110px:110px:110px][70px:70px, grow][70px:70px, grow][120px:120px:120px][70px:70px, grow]", "[][][100px:100px, grow][][130px:130px, grow][70px:70px, grow][][15px:15px][]"));
+
+		lblRecipeRefAdd = new JLabel("Recipe Ref:");
+			
+		textRecipeRefAdd = new JTextField();
+		textRecipeRefAdd.setEditable(false);
+
+		lblRecipeNameAdd = new JLabel("Recipe Name:");
+		tabbedRecipeAddPanel.add(lblRecipeNameAdd, "flowx,cell 0 0,alignx right");
+			
+		textRecipeNameAdd = new JTextField();
+		textRecipeNameAdd.setEditable(false);
+		tabbedRecipeAddPanel.add(textRecipeNameAdd, "cell 1 0 4,growx");
+
+		lblInspirationAdd = new JLabel("Inspiration:");
+		tabbedRecipeAddPanel.add(lblInspirationAdd, "flowx,cell 0 1,alignx right");
+			
+		textInspirationAdd = new JTextField();
+		textInspirationAdd.setEditable(false);
+		tabbedRecipeAddPanel.add(textInspirationAdd, "cell 1 1 4,growx");
+
+		lblIngredientsAdd = new JLabel("Ingredients:");
+		tabbedRecipeAddPanel.add(lblIngredientsAdd, "flowx,cell 0 2,alignx right");
+
+		textIngredientsAdd = new JTextArea();
+		textIngredientsAdd.setEditable(false);
+		textIngredientsAdd.setLineWrap(true);
+		textIngredientsAdd.setBackground(UIManager.getColor("Panel.background"));
+
+		//ScrollPane for textIngredientsB
+		RecipeAddIngredientsScrollPane = new JScrollPane();
+		RecipeAddIngredientsScrollPane.setViewportView(textIngredientsAdd);
+		tabbedRecipeAddPanel.add(RecipeAddIngredientsScrollPane, "cell 1 2 4,grow");
+
+		lblSuggestedYeastAdd = new JLabel("Suggested Yeast:");
+		tabbedRecipeAddPanel.add(lblSuggestedYeastAdd, "flowx,cell 0 3,alignx right");
+
+		textSuggestedYeastAdd = new JTextField();
+		textSuggestedYeastAdd.setEditable(false);
+		tabbedRecipeAddPanel.add(textSuggestedYeastAdd, "cell 1 3 2,growx");
+
+		lblVolumeAdd = new JLabel("Volume:");
+		tabbedRecipeAddPanel.add(lblVolumeAdd, "flowx,cell 3 3,alignx right");
+			
+		textVolumeAdd = new JTextField();
+		textVolumeAdd.setEditable(false);
+		tabbedRecipeAddPanel.add(textVolumeAdd, "cell 4 3,growx");
+
+		lblMethodAdd = new JLabel("Method:");
+		tabbedRecipeAddPanel.add(lblMethodAdd, "flowx,cell 0 4,alignx right");
+
+		textMethodAdd = new JTextArea();
+		textMethodAdd.setEditable(false);
+		textMethodAdd.setLineWrap(true);
+		textMethodAdd.setBackground(UIManager.getColor("Panel.background"));
+
+		//ScrollPane for textMethodB
+		RecipeAddMethodScrollPane = new JScrollPane();
+		RecipeAddMethodScrollPane.setViewportView(textMethodAdd);
+		tabbedRecipeAddPanel.add(RecipeAddMethodScrollPane, "cell 1 4 4,grow");
+
+		lblNotesAdd = new JLabel("Notes:");
+		tabbedRecipeAddPanel.add(lblNotesAdd, "flowx,cell 0 5,alignx right");
+
+		textNotesAdd = new JTextArea();
+		textNotesAdd.setEditable(false);
+		textNotesAdd.setLineWrap(true);
+		textNotesAdd.setBackground(UIManager.getColor("Panel.background"));
+
+		//ScrollPane for textNotesB
+		RecipeAddNotesScrollPane = new JScrollPane();
+		RecipeAddNotesScrollPane.setViewportView(textNotesAdd);
+		tabbedRecipeAddPanel.add(RecipeAddNotesScrollPane, "cell 1 5 4,grow");
+
+		lblReferencesAdd = new JLabel("References:");
+		tabbedRecipeAddPanel.add(lblReferencesAdd, "flowx,cell 0 6,alignx right");
+			
+		textReferencesAdd = new JTextField();
+		textReferencesAdd.setEditable(false);
+		tabbedRecipeAddPanel.add(textReferencesAdd, "cell 1 6 4,growx");
+
+		btnRecipeAddEdit = new JButton("Add");
+		btnRecipeAddEdit.setEnabled(true);
+		tabbedRecipeAddPanel.add(btnRecipeAddEdit, "cell 0 8,growx");
+
+		btnRecipeAddCancel = new JButton("Cancel");
+		btnRecipeAddCancel.setEnabled(false);
+		tabbedRecipeAddPanel.add(btnRecipeAddCancel, "cell 3 8,growx");
+			
+		btnRecipeAddSave = new JButton("Save / Insert");
+		btnRecipeAddSave.setEnabled(false);
+		tabbedRecipeAddPanel.add(btnRecipeAddSave, "cell 4 8,growx");
+		
+		//Add button listeners
+		btnRecipeAddEdit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnRecipeAddEdit.setEnabled(false);
+				btnRecipeAddCancel.setEnabled(true);
+				btnRecipeAddSave.setEnabled(true);
+				RecipePanel.tabbedRecipePane.setEnabledAt(0, false);
+				RecipePanel.tabbedRecipePane.setEnabledAt(1, false);
+				RecipePanel.tabbedRecipePane.setEnabledAt(2, true);
+				InitializeMenu.DisableAllMenuButtons();
+				textRecipeNameAdd.setEditable(true);
+				textInspirationAdd.setEditable(true);
+				textIngredientsAdd.setEditable(true);
+				textSuggestedYeastAdd.setEditable(true);
+				textMethodAdd.setEditable(true);
+				textNotesAdd.setEditable(true);
+				textReferencesAdd.setEditable(true);
+				textVolumeAdd.setEditable(true);
+				textIngredientsAdd.setBackground(Color.WHITE);
+				textMethodAdd.setBackground(Color.WHITE);
+				textNotesAdd.setBackground(Color.WHITE);
+			}
+		});
+		
+		btnRecipeAddCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnRecipeAddEdit.setEnabled(true);
+				btnRecipeAddCancel.setEnabled(false);
+				btnRecipeAddSave.setEnabled(false);
+				RecipePanel.tabbedRecipePane.setEnabledAt(0, true);
+				RecipePanel.tabbedRecipePane.setEnabledAt(1, false);
+				RecipePanel.tabbedRecipePane.setEnabledAt(2, true);
+				InitializeMenu.EnableAllMenuButtons();
+				textRecipeNameAdd.setEditable(false);
+				textInspirationAdd.setEditable(false);
+				textIngredientsAdd.setEditable(false);
+				textSuggestedYeastAdd.setEditable(false);
+				textMethodAdd.setEditable(false);
+				textNotesAdd.setEditable(false);
+				textReferencesAdd.setEditable(false);
+				textVolumeAdd.setEditable(false);
+				textIngredientsAdd.setBackground(UIManager.getColor("Panel.background"));
+				textMethodAdd.setBackground(UIManager.getColor("Panel.background"));
+				textNotesAdd.setBackground(UIManager.getColor("Panel.background"));
+				clearRecipeAddData();
+			}
+		});
+		
+		btnRecipeAddSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textRecipeNameAdd.getText().equals("") || textRecipeNameAdd.getText() == null){
+					JOptionPane.showMessageDialog(null,
+						"You must enter a recipe name.",
+						"Error",
+						JOptionPane.ERROR_MESSAGE
+					);
+				} else {
+					try {
+						DBEngine.addRecipe();
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+					btnRecipeAddEdit.setEnabled(true);
+					btnRecipeAddCancel.setEnabled(false);
+					btnRecipeAddSave.setEnabled(false);
+					RecipePanel.tabbedRecipePane.setEnabledAt(0, true);
+					RecipePanel.tabbedRecipePane.setEnabledAt(1, false);
+					RecipePanel.tabbedRecipePane.setEnabledAt(2, true);
+					InitializeMenu.EnableAllMenuButtons();
+					textRecipeNameAdd.setEditable(false);
+					textInspirationAdd.setEditable(false);
+					textIngredientsAdd.setEditable(false);
+					textSuggestedYeastAdd.setEditable(false);
+					textMethodAdd.setEditable(false);
+					textNotesAdd.setEditable(false);
+					textReferencesAdd.setEditable(false);
+					textVolumeAdd.setEditable(false);
+					textIngredientsAdd.setBackground(UIManager.getColor("Panel.background"));
+					textMethodAdd.setBackground(UIManager.getColor("Panel.background"));
+					textNotesAdd.setBackground(UIManager.getColor("Panel.background"));
+					clearRecipeAddData();
+				}
+			}
+		});
+	}
+	
+	public static void clearRecipeAddData(){
+		textRecipeRefAdd.setText("");
+		textRecipeNameAdd.setText("");
+		textInspirationAdd.setText("");
+		textIngredientsAdd.setText("");
+		textSuggestedYeastAdd.setText("");
+		textMethodAdd.setText("");
+		textNotesAdd.setText("");
+		textReferencesAdd.setText("");
+		textVolumeAdd.setText("");
+	}
+	
+	public static boolean stringToBool(String s) {
+		  if (s.equals("1"))
+		    return true;
+		  if (s.equals("0"))
+		    return false;
+		  throw new IllegalArgumentException(s+" is not a bool. Only 1 and 0 are.");
+		}
+
+
+}
--- winebrewdb/src/com/pori/WineBrewDB/RecipePanel.java
+++ winebrewdb/src/com/pori/WineBrewDB/RecipePanel.java
@@ -1,31 +1,27 @@
 package com.pori.WineBrewDB;
 
-import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JTabbedPane;
 
 import net.miginfocom.swing.MigLayout;
-import javax.swing.JEditorPane;
 
 public class RecipePanel extends JPanel {
 
-	private static final long serialVersionUID = -7290586613655558447L;
+	private static final long serialVersionUID = 1332318777984759664L;
 	public static JPanel RecipePanel;
-	public static JEditorPane RecipeText;
 	public static JLabel RecipeHeader;
 	public static JLabel RecipeSubtitle;
-	public static JTextField txtRecipePanel;
+	public static JTabbedPane tabbedRecipePane;
 	public static String RecipePanelStatus = "DeInitialized";
 
 	//public RecipePanel() {
 	public static void InitializePanel(){
 		
-		//TODO: Finish recipe panels.
 		RecipePanel = new JPanel();
-		RecipePanel.setLayout(new MigLayout("", "[grow][100px:n:100px]", "[20px:n:20px][25px:n:25px][grow][50px:n:50px]"));
+		RecipePanel.setLayout(new MigLayout("", "[grow]", "[20px:n:20px][25px:n:25px][grow]"));
 		
 		
 		//Header
@@ -35,34 +31,53 @@ public class RecipePanel extends JPanel {
 		
 		
 		//Subtitle
-		RecipeSubtitle = new JLabel("Search for recipes.");
+		RecipeSubtitle = new JLabel("Browse and edit the Recipe database.");
 		RecipeSubtitle.setFont(new Font("Tahoma", Font.ITALIC, 13));
 		RecipePanel.add(RecipeSubtitle, "cell 0 1,growx,aligny top");
 		
 		
-		//Some content
-		txtRecipePanel = new JTextField();
-		txtRecipePanel.setBackground(Color.WHITE);
-		txtRecipePanel.setText("RecipePanel - Something will go here to search for/filter recipes entered in the database, and view their details.");
-		txtRecipePanel.setColumns(10);
-		RecipePanel.add(txtRecipePanel, "cell 0 2,grow");
+		//Tabbed Pane
+		tabbedRecipePane = new JTabbedPane(JTabbedPane.TOP);
+		RecipePanel.add(tabbedRecipePane, "cell 0 2,grow");
+				
+		
+		//Recipe Search Tab
+		RecipeSearchPanel.InitializePanel();		
+		tabbedRecipePane.addTab("Search", null, RecipeSearchPanel.tabbedRecipeSearchPanel, null);
 		
 		
+		//Recipe Data Tab
+		RecipeDataPanel.InitializePanel();		
+		tabbedRecipePane.addTab("Recipe Data", null, RecipeDataPanel.tabbedRecipeDataPanel, null);
+		
+		
+		//Add New Recipe Tab
+		RecipeAddPanel.InitializePanel();
+		tabbedRecipePane.addTab("Add New Recipe", null, RecipeAddPanel.tabbedRecipeAddPanel, null);
+		
+		
+		//Set some tabs disabled initially
+		tabbedRecipePane.setEnabledAt(1, false);
+		
+	   	
 		//Add it all to the main window
 		MainWindow.WineBrewDBFrame.getContentPane().add(RecipePanel, "cell 0 0,grow");
 		RecipePanel.setVisible(false);
 
-		RecipePanelStatus = "Initialized";
 		
+		RecipePanelStatus = "Initialized";
 	}
-	
 
+	
 	public static void DeInitializePanel(){
 		if(RecipePanelStatus.equals("Initialized")) {
 			RecipePanel.setVisible(false);
 			RecipePanel.remove(RecipeHeader);
 			RecipePanel.remove(RecipeSubtitle);
-			RecipePanel.remove(txtRecipePanel);
+			RecipePanel.remove(tabbedRecipePane);
+			tabbedRecipePane.remove(RecipeSearchPanel.tabbedRecipeSearchPanel);
+			tabbedRecipePane.remove(RecipeDataPanel.tabbedRecipeDataPanel);
+			tabbedRecipePane.remove(RecipeAddPanel.tabbedRecipeAddPanel);
 			MainWindow.WineBrewDBFrame.getContentPane().remove(RecipePanel);
 			RecipePanelStatus = "DeInitialized";
 		}
