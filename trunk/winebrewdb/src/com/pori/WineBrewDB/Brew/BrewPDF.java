d39a24a122361f5772bc3e076e127a3579920e3f
treguard0439@gmail.com <treguard0439@gmail.com@882ba1e9-8c69-7e79-3acf-8f0a333cfdfb>
2012-03-22 12:40:18 +0000

    BrewPDF mostly done (minus pictures).

--- trunk/winebrewdb/src/com/pori/WineBrewDB/Brew/BrewPDF.java
+++ trunk/winebrewdb/src/com/pori/WineBrewDB/Brew/BrewPDF.java
@@ -3,6 +3,7 @@ package com.pori.WineBrewDB.Brew;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.text.NumberFormat;
 
 import javax.swing.JOptionPane;
 
@@ -22,6 +23,7 @@ import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfPageEventHelper;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.pdf.draw.LineSeparator;
+import com.itextpdf.text.pdf.draw.VerticalPositionMark;
 import com.pori.WineBrewDB.Dates;
 
 
@@ -29,9 +31,10 @@ public class BrewPDF {
 	
 	private static Font heading1Font = new Font(Font.FontFamily.HELVETICA, 18,Font.BOLD);
 	private static Font text2 = new Font(Font.FontFamily.HELVETICA, 12,Font.ITALIC);
+	private static Font text3 = new Font(Font.FontFamily.HELVETICA, 12,Font.BOLD);
 	private static Font smallBold = new Font(Font.FontFamily.HELVETICA, 9,Font.BOLD);
 	private static Font smallNormal = new Font(Font.FontFamily.HELVETICA, 9,Font.NORMAL);
-	private static Font smallItalic = new Font(Font.FontFamily.HELVETICA, 9,Font.ITALIC);
+	//private static Font smallItalic = new Font(Font.FontFamily.HELVETICA, 9,Font.ITALIC);
 	private static Font headerFoot = new Font(Font.FontFamily.HELVETICA, 7,Font.ITALIC,BaseColor.GRAY);
 	
 
@@ -108,19 +111,110 @@ public class BrewPDF {
 			throws DocumentException {
 		Paragraph genericParagraph = new Paragraph();
 		LineSeparator ls = new LineSeparator();
+		Chunk tab1 = new Chunk(new VerticalPositionMark(), 75, true);
+		Chunk tab2 = new Chunk(new VerticalPositionMark(), 170, true);
+		Chunk tab3 = new Chunk(new VerticalPositionMark(), 275, true);
+		Chunk tab4 = new Chunk(new VerticalPositionMark(), 350, true);
+		Chunk tab5 = new Chunk(new VerticalPositionMark(), 455, true);
 		
 		//Empty Line
 		addEmptyLine(genericParagraph, 1);
 		
 		//Header
 		genericParagraph.add(new Paragraph(BrewDataPanel.textBrewNameB.getText(), heading1Font));
-		genericParagraph.add(new Paragraph("Date Planned: " + Dates.dateToString(BrewDataPanel.chooserBrewDatePlannedB.getDate()) + "          Date Started: " + Dates.dateToString(BrewDataPanel.chooserBrewDateStartedB.getDate()) + "          Date Bottled: " + Dates.dateToString(BrewDataPanel.chooserBrewDateBottledB.getDate()), text2));
-		genericParagraph.add(new Chunk(ls));
+		genericParagraph.add(new Phrase("Date Planned: " + Dates.dateToString(BrewDataPanel.chooserBrewDatePlannedB.getDate()), text2));
+		genericParagraph.add(new Chunk(tab2));
+		genericParagraph.add(new Phrase("Date Started: " + Dates.dateToString(BrewDataPanel.chooserBrewDateStartedB.getDate()), text2));
+		genericParagraph.add(new Chunk(tab4));
+		genericParagraph.add(new Phrase("Date Bottled: " + Dates.dateToString(BrewDataPanel.chooserBrewDateBottledB.getDate()), text2));
 		addEmptyLine(genericParagraph, 1);
+		genericParagraph.add(new Chunk(ls));
+		addEmptyLine(genericParagraph, 2);
 		
-		genericParagraph.add(new Paragraph("Recipe From:",smallBold));
-		addEmptyLine(genericParagraph, 3);
+		genericParagraph.add(new Phrase("Recipe From:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewRecipeB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 1);
+		genericParagraph.add(new Phrase("Yeast Used:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewYeastB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 2);
 		
+		genericParagraph.add(new Phrase("Colour:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		genericParagraph.add(new Phrase(BrewDataPanel.comboBrewColourB.getSelectedItem().toString(),smallNormal));
+		genericParagraph.add(new Chunk(tab2));
+		genericParagraph.add(new Phrase("Volume Made (gallons):",smallBold));
+		genericParagraph.add(new Chunk(tab3));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewVolumeMadeB.getText(),smallNormal));
+		genericParagraph.add(new Chunk(tab4));
+		genericParagraph.add(new Phrase("Number of Bottles:",smallBold));
+		genericParagraph.add(new Chunk(tab5));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewNumberBottlesB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Phrase("Current Status:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		String currentStatus = "";
+		if(BrewDataPanel.chckbxBrewInPlanningB.isSelected()){
+			currentStatus = currentStatus + "*In Planning*  ";
+		}
+		if(BrewDataPanel.chckbxBrewInFermentingB.isSelected()){
+			currentStatus = currentStatus + "*In Fermenting*  ";
+		}
+		if(BrewDataPanel.chckbxBrewInFiningB.isSelected()){
+			currentStatus = currentStatus + "*In Fining*  ";
+		}
+		if(BrewDataPanel.chckbxBrewInMaturingB.isSelected()){
+			currentStatus = currentStatus + "*In Maturing*  ";
+		}
+		if(BrewDataPanel.chckbxBrewInBottlesB.isSelected()){
+			currentStatus = currentStatus + "*In Bottles*  ";
+		}
+		if(BrewDataPanel.chckbxBrewDrunkB.isSelected()){
+			currentStatus = currentStatus + "*Drunk*  ";
+		}
+		genericParagraph.add(new Phrase(currentStatus,smallNormal));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Chunk(ls));
+		addEmptyLine(genericParagraph, 2);
+		
+		genericParagraph.add(new Phrase("Start SG:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewStartSGB.getText().toString(),smallNormal));
+		genericParagraph.add(new Chunk(tab2));
+		genericParagraph.add(new Phrase("Start Adjusted SG:",smallBold));
+		genericParagraph.add(new Chunk(tab3));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewStartAdjustedSGB.getText(),smallNormal));
+		genericParagraph.add(new Chunk(tab4));
+		genericParagraph.add(new Phrase("End SG:",smallBold));
+		genericParagraph.add(new Chunk(tab5));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewEndSGB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Phrase("Aimed ABV%:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewAimedABVB.getText().toString(),smallNormal));
+		genericParagraph.add(new Chunk(tab2));
+		genericParagraph.add(new Phrase("Final ABV%:",smallBold));
+		genericParagraph.add(new Chunk(tab3));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewFinalABVB.getText(),smallNormal));
+		genericParagraph.add(new Chunk(tab4));
+		genericParagraph.add(new Phrase("Final Adjusted ABV%:",smallBold));
+		genericParagraph.add(new Chunk(tab5));
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewFinalAdjustedABVB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Chunk(ls));
+		addEmptyLine(genericParagraph, 2);
+		
+		genericParagraph.add(new Phrase("Tasting Notes:",smallBold));
+		addEmptyLine(genericParagraph, 1);
+		genericParagraph.add(new Phrase(BrewDataPanel.textBrewTastingNotesB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Phrase("Thumbs:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		genericParagraph.add(new Phrase(BrewDataPanel.comboBrewThumbsB.getSelectedItem().toString(),smallNormal));
+		addEmptyLine(genericParagraph, 2);		
+		genericParagraph.add(new Phrase("General Notes:",smallBold));
+		addEmptyLine(genericParagraph, 1);
 		genericParagraph.add(new Paragraph(BrewDataPanel.textBrewGeneralNotesB.getText(),smallNormal));
 	
 		// Now add all this to the document
@@ -136,7 +230,7 @@ public class BrewPDF {
 		//Empty Line
 		addEmptyLine(genericParagraph, 1);
 		//Header
-		genericParagraph.add(new Paragraph("Brew Notes", heading1Font));
+		genericParagraph.add(new Paragraph("Brew Notes", text3));
 		addEmptyLine(genericParagraph, 1);
 		// Add a table
 		createNotesTable(genericParagraph);
@@ -155,7 +249,7 @@ public class BrewPDF {
 		//Empty Line
 		addEmptyLine(genericParagraph, 1);
 		//Header
-		genericParagraph.add(new Paragraph("Brew Costs", heading1Font));
+		genericParagraph.add(new Paragraph("Brew Costs", text3));
 		addEmptyLine(genericParagraph, 1);
 		// Add a table
 		createCostsTable(genericParagraph);
@@ -174,7 +268,7 @@ public class BrewPDF {
 		//Empty Line
 		addEmptyLine(genericParagraph, 1);
 		//Header
-		genericParagraph.add(new Paragraph("Brew Pictures", heading1Font));
+		genericParagraph.add(new Paragraph("Brew Pictures", text3));
 		addEmptyLine(genericParagraph, 1);
 		// Add a table
 		createPicturesTable(genericParagraph);
@@ -190,32 +284,46 @@ public class BrewPDF {
 	
 	private static void createNotesTable(Paragraph preface)
 			throws BadElementException {
-		PdfPTable table = new PdfPTable(3);
+		PdfPTable table = new PdfPTable(4);
+        try {
+			table.setTotalWidth(new float[]{ 67, 80, 180, 210 });
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+        table.setLockedWidth(true);
+		
+		table.getDefaultCell().setPadding(10);
+		
+		// table.setBorderWidth(1);
 	
-		// t.setBorderColor(BaseColor.GRAY);
-		// t.setPadding(4);
-		// t.setSpacing(4);
-		// t.setBorderWidth(1);
-	
-		PdfPCell c1 = new PdfPCell(new Phrase("Table Header 1"));
+		PdfPCell c1 = new PdfPCell(new Phrase("Date",smallBold));
 		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
 		table.addCell(c1);
 	
-		c1 = new PdfPCell(new Phrase("Table Header 2"));
+		c1 = new PdfPCell(new Phrase("Days Since Start",smallBold));
 		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
 		table.addCell(c1);
 	
-		c1 = new PdfPCell(new Phrase("Table Header 3"));
+		c1 = new PdfPCell(new Phrase("Incident",smallBold));
+		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+		table.addCell(c1);
+		
+		c1 = new PdfPCell(new Phrase("Notes",smallBold));
 		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
 		table.addCell(c1);
 		table.setHeaderRows(1);
-	
-		table.addCell("1.0");
-		table.addCell("1.1");
-		table.addCell("1.2");
-		table.addCell("2.1");
-		table.addCell("2.2");
-		table.addCell("2.3");
+		
+		int totalRows = BrewNotesPanel.BrewNotesTable.getRowCount();
+		int row = 0;
+		
+		while(row < totalRows) {
+			table.addCell(new Phrase(BrewNotesPanel.BrewNotesTable.getValueAt(row,1).toString(),smallNormal));
+			table.addCell(new Phrase(BrewNotesPanel.BrewNotesTable.getValueAt(row,2).toString(),smallNormal));
+			table.addCell(new Phrase(BrewNotesPanel.BrewNotesTable.getValueAt(row,3).toString(),smallNormal));
+			table.addCell(new Phrase(BrewNotesPanel.BrewNotesTable.getValueAt(row,4).toString(),smallNormal));
+			
+			row = row + 1;
+		}
 	
 		preface.add(table);
 	
@@ -225,31 +333,40 @@ public class BrewPDF {
 	private static void createCostsTable(Paragraph preface)
 			throws BadElementException {
 		PdfPTable table = new PdfPTable(3);
+        try {
+			table.setTotalWidth(new float[]{ 233, 70, 234 });
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+        table.setLockedWidth(true);
+		
+		table.getDefaultCell().setPadding(10);
+		
+		// table.setBorderWidth(1);
 	
-		// t.setBorderColor(BaseColor.GRAY);
-		// t.setPadding(4);
-		// t.setSpacing(4);
-		// t.setBorderWidth(1);
-	
-		PdfPCell c1 = new PdfPCell(new Phrase("Table Header 1"));
+		PdfPCell c1 = new PdfPCell(new Phrase("Line Item",smallBold));
 		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
 		table.addCell(c1);
 	
-		c1 = new PdfPCell(new Phrase("Table Header 2"));
+		c1 = new PdfPCell(new Phrase("Cost",smallBold));
 		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
 		table.addCell(c1);
 	
-		c1 = new PdfPCell(new Phrase("Table Header 3"));
+		c1 = new PdfPCell(new Phrase("Supplier",smallBold));
 		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
 		table.addCell(c1);
-		table.setHeaderRows(1);
-	
-		table.addCell("1.0");
-		table.addCell("1.1");
-		table.addCell("1.2");
-		table.addCell("2.1");
-		table.addCell("2.2");
-		table.addCell("2.3");
+		
+		int totalRows = BrewCostPanel.BrewCostTable.getRowCount();
+		int row = 0;
+		
+		while(row < totalRows) {
+			NumberFormat nf = NumberFormat.getCurrencyInstance();
+			table.addCell(new Phrase(BrewCostPanel.BrewCostTable.getValueAt(row,1).toString(),smallNormal));
+			table.addCell(new Phrase(nf.format(BrewCostPanel.BrewCostTable.getValueAt(row,2)),smallNormal));
+			table.addCell(new Phrase(BrewCostPanel.BrewCostTable.getValueAt(row,3).toString(),smallNormal));
+			
+			row = row + 1;
+		}
 	
 		preface.add(table);
 	
@@ -258,32 +375,46 @@ public class BrewPDF {
 	
 	private static void createPicturesTable(Paragraph preface)
 			throws BadElementException {
-		PdfPTable table = new PdfPTable(3);
+		PdfPTable table = new PdfPTable(4);
+        try {
+			table.setTotalWidth(new float[]{ 67, 80, 180, 210 });
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+        table.setLockedWidth(true);
+		
+		table.getDefaultCell().setPadding(10);
+		
+		// table.setBorderWidth(1);
 	
-		// t.setBorderColor(BaseColor.GRAY);
-		// t.setPadding(4);
-		// t.setSpacing(4);
-		// t.setBorderWidth(1);
-	
-		PdfPCell c1 = new PdfPCell(new Phrase("Table Header 1"));
+		PdfPCell c1 = new PdfPCell(new Phrase("Date",smallBold));
 		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
 		table.addCell(c1);
 	
-		c1 = new PdfPCell(new Phrase("Table Header 2"));
+		c1 = new PdfPCell(new Phrase("Days Since Start",smallBold));
 		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
 		table.addCell(c1);
 	
-		c1 = new PdfPCell(new Phrase("Table Header 3"));
+		c1 = new PdfPCell(new Phrase("Incident",smallBold));
+		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+		table.addCell(c1);
+		
+		c1 = new PdfPCell(new Phrase("Notes",smallBold));
 		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
 		table.addCell(c1);
 		table.setHeaderRows(1);
-	
-		table.addCell("1.0");
-		table.addCell("1.1");
-		table.addCell("1.2");
-		table.addCell("2.1");
-		table.addCell("2.2");
-		table.addCell("2.3");
+		
+		int totalRows = BrewNotesPanel.BrewNotesTable.getRowCount();
+		int row = 0;
+		
+		while(row < totalRows) {
+			table.addCell(new Phrase(BrewNotesPanel.BrewNotesTable.getValueAt(row,1).toString(),smallNormal));
+			table.addCell(new Phrase(BrewNotesPanel.BrewNotesTable.getValueAt(row,2).toString(),smallNormal));
+			table.addCell(new Phrase(BrewNotesPanel.BrewNotesTable.getValueAt(row,3).toString(),smallNormal));
+			table.addCell(new Phrase(BrewNotesPanel.BrewNotesTable.getValueAt(row,4).toString(),smallNormal));
+			
+			row = row + 1;
+		}
 	
 		preface.add(table);
 	
