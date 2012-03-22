86050fed4ed9aeeff3bf9720785dbd6724c68d5c
Paul Bellchambers <pbellchambers@users.noreply.github.com>
2012-03-22 16:59:52 +0000

    Tidied up required jars, and done recipe pdf printing.

--- winebrewdb/src/com/pori/WineBrewDB/Recipe/RecipePDF.java
+++ winebrewdb/src/com/pori/WineBrewDB/Recipe/RecipePDF.java
@@ -6,31 +6,28 @@ import java.io.FileOutputStream;
 
 import javax.swing.JOptionPane;
 
-import com.itextpdf.text.Anchor;
-import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.Chapter;
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
-import com.itextpdf.text.List;
-import com.itextpdf.text.ListItem;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
-import com.itextpdf.text.Section;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfPageEventHelper;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.pori.WineBrewDB.Brew.BrewDataPanel;
+import com.itextpdf.text.pdf.draw.LineSeparator;
+import com.itextpdf.text.pdf.draw.VerticalPositionMark;
 
 
 public class RecipePDF {
  
-	private static Font catFont = new Font(Font.FontFamily.HELVETICA, 18,Font.BOLD);
-	private static Font redFont = new Font(Font.FontFamily.HELVETICA, 12,Font.NORMAL,BaseColor.RED);
-	private static Font subFont = new Font(Font.FontFamily.HELVETICA, 16,Font.BOLD);
-	private static Font smallBold = new Font(Font.FontFamily.HELVETICA, 12,Font.BOLD);
+	private static Font heading1Font = new Font(Font.FontFamily.HELVETICA, 18,Font.BOLD);
+	private static Font smallBold = new Font(Font.FontFamily.HELVETICA, 9,Font.BOLD);
+	private static Font smallNormal = new Font(Font.FontFamily.HELVETICA, 9,Font.NORMAL);
+	private static Font headerFoot = new Font(Font.FontFamily.HELVETICA, 7,Font.ITALIC,BaseColor.GRAY);
 	
 
 	private static void openPDF(String location) {
@@ -62,16 +59,20 @@ public class RecipePDF {
  
 	}
 	
-	
+
 	public static void createPDF(String location) {
 
 		try {
 				Document document = new Document();
-				PdfWriter.getInstance(document, new FileOutputStream(location));
+				PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(location));
+				writer.setBoxSize("box", new Rectangle(36, 36, 559, 815));
+
+	            HeaderFooter event = new HeaderFooter();
+	            writer.setPageEvent(event);
+				
 				document.open();
 				addMetaData(document);
-				addTitlePage(document);
-				addContent(document);
+				addBrewData(document);
 				document.close();
 				openPDF(location);
 			} catch (Exception e) {
@@ -85,9 +86,7 @@ public class RecipePDF {
 
 
 
-	// iText allows to add metadata to the PDF which can be viewed in your Adobe
-	// Reader
-	// under File -> Properties
+	//iText allows to add metadata to the PDF which can be viewed in your Adobe Reader under File -> Properties
 	private static void addMetaData(Document document) {
 		document.addTitle(RecipeDataPanel.textRecipeNameB.getText());
 		document.addSubject("Recipe Data for recipe: " + RecipeDataPanel.textRecipeNameB.getText());
@@ -96,122 +95,68 @@ public class RecipePDF {
 		document.addCreator("WineBrewDB");
 	}
 	
-	private static void addTitlePage(Document document)
+	
+	private static void addBrewData(Document document)
 			throws DocumentException {
-		Paragraph preface = new Paragraph();
-		// We add one empty line
-		addEmptyLine(preface, 1);
-		// Lets write a big header
-		preface.add(new Paragraph("Title of the document", catFont));
+		Paragraph genericParagraph = new Paragraph();
+		LineSeparator ls = new LineSeparator();
+		Chunk tab1 = new Chunk(new VerticalPositionMark(), 85, true);
+		Chunk tab4 = new Chunk(new VerticalPositionMark(), 380, true);
+		Chunk tab5 = new Chunk(new VerticalPositionMark(), 465, true);
+		
+		//Empty Line
+		addEmptyLine(genericParagraph, 1);
+		
+		//Header
+		genericParagraph.add(new Paragraph(RecipeDataPanel.textRecipeNameB.getText(), heading1Font));
+		addEmptyLine(genericParagraph, 1);
+		genericParagraph.add(new Chunk(ls));
+		addEmptyLine(genericParagraph, 2);
+		
+		genericParagraph.add(new Phrase("Inspiration:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		genericParagraph.add(new Phrase(RecipeDataPanel.textInspirationB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 1);
+		genericParagraph.add(new Phrase("Suggested Yeast:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		genericParagraph.add(new Phrase(RecipeDataPanel.textSuggestedYeastB.getText(),smallNormal));
+		genericParagraph.add(new Chunk(tab4));
+		genericParagraph.add(new Phrase("Volume (gallons):",smallBold));
+		genericParagraph.add(new Chunk(tab5));
+		genericParagraph.add(new Phrase(RecipeDataPanel.textVolumeB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Chunk(ls));
+		addEmptyLine(genericParagraph, 2);		
+
+		genericParagraph.add(new Phrase("Ingredients:",smallBold));
+		addEmptyLine(genericParagraph, 1);
+		genericParagraph.add(new Phrase(RecipeDataPanel.textIngredientsB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Chunk(ls));
+		addEmptyLine(genericParagraph, 2);
+		
+		genericParagraph.add(new Phrase("Method:",smallBold));
+		addEmptyLine(genericParagraph, 1);
+		genericParagraph.add(new Phrase(RecipeDataPanel.textMethodB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Chunk(ls));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Phrase("Notes:",smallBold));
+		addEmptyLine(genericParagraph, 1);
+		genericParagraph.add(new Phrase(RecipeDataPanel.textNotesB.getText(),smallNormal));
+		addEmptyLine(genericParagraph, 2);
+		genericParagraph.add(new Phrase("References:",smallBold));
+		genericParagraph.add(new Chunk(tab1));
+		genericParagraph.add(new Phrase(RecipeDataPanel.textReferencesB.getText(),smallNormal));
+		
 	
-		addEmptyLine(preface, 1);
-		// Will create: Report generated by: _name, _date
-		preface.add(new Paragraph(
-				"Report generated by: " + System.getProperty("user.name") + ", ",
-				smallBold));
-		addEmptyLine(preface, 3);
-		preface.add(new Paragraph(
-				BrewDataPanel.textBrewGeneralNotesB.getText(),
-				smallBold));
-	
-		addEmptyLine(preface, 8);
-	
-		preface.add(new Paragraph(
-				"Test red font ;-).",
-				redFont));
-	
-		document.add(preface);
+		// Now add all this to the document
+		document.add(genericParagraph);
+		
 		// Start a new page
 		document.newPage();
 	}
 	
-	private static void addContent(Document document) throws DocumentException {
-		Anchor anchor = new Anchor("First Chapter", catFont);
-		anchor.setName("First Chapter");
-	
-		// Second parameter is the number of the chapter
-		Chapter catPart = new Chapter(new Paragraph(anchor), 1);
-	
-		Paragraph subPara = new Paragraph("Subcategory 1", subFont);
-		Section subCatPart = catPart.addSection(subPara);
-		subCatPart.add(new Paragraph("Hello"));
-	
-		subPara = new Paragraph("Subcategory 2", subFont);
-		subPara.setAlignment(Element.ALIGN_RIGHT);
-		subPara.setIndentationRight(50);
-		subCatPart = catPart.addSection(subPara);
-		subCatPart.add(new Paragraph("Paragraph 1"));
-		subCatPart.add(new Paragraph("Paragraph 2"));
-		subCatPart.add(new Paragraph("Paragraph 3"));
-	
-		// Add a list
-		createList(subCatPart);
-		Paragraph paragraph = new Paragraph();
-		addEmptyLine(paragraph, 5);
-		subCatPart.add(paragraph);
-	
-		// Add a table
-		createTable(subCatPart);
-	
-		// Now add all this to the document
-		document.add(catPart);
-	
-		// Next section
-		anchor = new Anchor("Second Chapter", catFont);
-		anchor.setName("Second Chapter");
-	
-		// Second parameter is the number of the chapter
-		catPart = new Chapter(new Paragraph(anchor), 1);
-	
-		subPara = new Paragraph("Subcategory", subFont);
-		subCatPart = catPart.addSection(subPara);
-		subCatPart.add(new Paragraph("This is a very important message"));
-	
-		// Now add all this to the document
-		document.add(catPart);
-	
-	}
-	
-	private static void createTable(Section subCatPart)
-			throws BadElementException {
-		PdfPTable table = new PdfPTable(3);
-	
-		// t.setBorderColor(BaseColor.GRAY);
-		// t.setPadding(4);
-		// t.setSpacing(4);
-		// t.setBorderWidth(1);
-	
-		PdfPCell c1 = new PdfPCell(new Phrase("Table Header 1"));
-		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-		table.addCell(c1);
-	
-		c1 = new PdfPCell(new Phrase("Table Header 2"));
-		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-		table.addCell(c1);
-	
-		c1 = new PdfPCell(new Phrase("Table Header 3"));
-		c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-		table.addCell(c1);
-		table.setHeaderRows(1);
-	
-		table.addCell("1.0");
-		table.addCell("1.1");
-		table.addCell("1.2");
-		table.addCell("2.1");
-		table.addCell("2.2");
-		table.addCell("2.3");
-	
-		subCatPart.add(table);
-	
-	}
-	
-	private static void createList(Section subCatPart) {
-		List list = new List(true, false, 10);
-		list.add(new ListItem("First point"));
-		list.add(new ListItem("Second point"));
-		list.add(new ListItem("Third point"));
-		subCatPart.add(list);
-	}
 	
 	private static void addEmptyLine(Paragraph paragraph, int number) {
 		for (int i = 0; i < number; i++) {
@@ -219,5 +164,16 @@ public class RecipePDF {
 		}
 	}
 	
+
+	static class HeaderFooter extends PdfPageEventHelper {
+
+        public void onEndPage (PdfWriter writer, Document document) {
+            Rectangle rect = writer.getBoxSize("box");
+            
+            ColumnText.showTextAligned(writer.getDirectContent(),Element.ALIGN_LEFT, new Phrase("WineBrewDB",headerFoot),rect.getLeft(), rect.getTop(), 0);
+            ColumnText.showTextAligned(writer.getDirectContent(),Element.ALIGN_RIGHT, new Phrase("Recipe Data: " + RecipeDataPanel.textRecipeNameB.getText(),headerFoot),rect.getRight(), rect.getTop(), 0);
+            ColumnText.showTextAligned(writer.getDirectContent(),Element.ALIGN_CENTER, new Phrase(String.format("Page %d", writer.getPageNumber()),headerFoot),(rect.getLeft() + rect.getRight()) / 2, rect.getBottom() - 18, 0);
+        }
+    }	
 	
 }
