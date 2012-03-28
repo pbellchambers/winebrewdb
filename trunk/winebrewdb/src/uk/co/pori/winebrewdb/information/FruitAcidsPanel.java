73bb8327c053a3b55b441b121913260be916e32f
treguard0439@gmail.com <treguard0439@gmail.com@882ba1e9-8c69-7e79-3acf-8f0a333cfdfb>
2012-03-28 13:08:40 +0000

    Finished refactoring (all lowercase package names now)

---
+++ trunk/winebrewdb/src/uk/co/pori/winebrewdb/information/FruitAcidsPanel.java
@@ -0,0 +1,60 @@
+package uk.co.pori.winebrewdb.information;
+
+import java.io.IOException;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import javax.swing.JEditorPane;
+import javax.swing.ScrollPaneConstants;
+
+import uk.co.pori.winebrewdb.MainWindow;
+
+
+public class FruitAcidsPanel extends JPanel {
+	
+	private static final long serialVersionUID = -6664688139205966142L;
+	static JScrollPane FruitAcidsScrollPane;
+	private static String FruitAcidsPanelStatus = "DeInitialized";
+
+	//public FruitAcidsPanel() {
+	public static void InitializePanel(){
+		
+		
+		//Text area that gets text from external html
+		JEditorPane FruitAcidsText = new JEditorPane();
+		FruitAcidsText.setEditable(false);
+		java.net.URL helpURL = FruitAcidsPanel.class.getResource("/uk/co/pori/winebrewdb/htmlcontent/FruitAcids.html");
+		if (helpURL != null) {
+			try {FruitAcidsText.setPage(helpURL);
+			} catch (IOException e) {
+				System.err.println("Attempted to read a bad URL: " + helpURL);
+			}
+		} else {
+			System.err.println("Couldn't find file: " + helpURL);
+		}
+		
+		//Put it in a scrollpane
+		FruitAcidsScrollPane = new JScrollPane(FruitAcidsText);
+		FruitAcidsScrollPane.setBorder(null);
+		FruitAcidsScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+		
+		//Add it all to the main window
+		MainWindow.WineBrewDBFrame.getContentPane().add(FruitAcidsScrollPane, "cell 0 0,grow");
+		FruitAcidsScrollPane.setVisible(false);
+
+		FruitAcidsPanelStatus = "Initialized";
+	}
+
+	
+	public static void DeInitializePanel(){
+		if(FruitAcidsPanelStatus.equals("Initialized")) {
+			FruitAcidsScrollPane.setVisible(false);
+			MainWindow.WineBrewDBFrame.getContentPane().remove(FruitAcidsScrollPane);
+			FruitAcidsPanelStatus = "DeInitialized";
+		}
+	}
+	
+
+}
